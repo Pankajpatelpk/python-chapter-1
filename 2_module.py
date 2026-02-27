cd551cf29fee51c6e72353e764a7a6495767ccff
Pankaj Patel <pankajsingh95697@gmail.com>
2026-02-27 19:48:28 +0530

    pyjokes in python

---
+++ 2_module.py
@@ -0,0 +1,9 @@
+import pyjokes
+# thids is my programming language
+
+joke=pyjokes.get_joke()
+print(joke)
+# multiline comment
+"""
+opekorihoifhgiosehrwe9ur-0suelkxmcxojfsdssdhaeawaeuAAPWOEJwqeuqw78   
+"""
